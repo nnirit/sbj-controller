007832837caa6d02772624eb0c9fd6bc53555b17
michalvadai <michalvadai@ce34eef6-3686-6dd4-c0f4-36e7464ae2ac>
2013-03-04 14:09:09 +0000

    Added DataChannel class to represents different types of channels to be sampled.

---
+++ sbj-controller/SBJController/SBJController/IDataChannel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SBJController
+{
+    public interface IDataChannel
+    {
+        string PhysicalName { get; set; }
+        string Name { get; set; }
+        IList<double[]> RawData { get; set; }
+        IList<double[]> PhysicalData { get; set; }
+        DataConvertorSettings DataConvertionSettings { get; set; }
+
+
+
+        IList<double[]> ConvertToPhysicalData();
+    }
+}
