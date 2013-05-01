067a108a72aab263147b0d06f412139591eb6e8b
nnirit@gmail.com <nnirit@gmail.com@ce34eef6-3686-6dd4-c0f4-36e7464ae2ac>
2013-05-01 08:28:18 +0000

    adding Attributes file for the classification of the channels

---
+++ trunk/sbj-controller/SBJController/SBJController/Attributes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SBJController
+{
+    /// <summary>
+    /// This class is an attribute that marks channels that should be used by the main DAQ tab
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class DAQAttribute : System.Attribute
+    {
+        public DAQAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// This class is an attribute that marks channels that should be used by the IV tab
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class IVAttribute : System.Attribute 
+    {
+        public IVAttribute()
+        { 
+        }
+    }
+
+    /// <summary>
+    /// This class is an attribute that marks channels that should be used by the Calibration tab
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class CalibrationAttribute : System.Attribute
+    {
+        public CalibrationAttribute()
+        {
+        }
+    }
+
+}
